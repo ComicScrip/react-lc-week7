27156f472adf237003304de0e39b155a9243502c
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2022-04-12 14:51:50 +0200

    display the list of articles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,21 @@
+import { NavLink } from 'react-router-dom';
+
+const getNavlinkStyle = ({ isActive }) => ({
+  color: isActive ? 'orange' : 'white',
+});
+
 export default function Header() {
-  return <header>header</header>;
+  return (
+    <header>
+      <nav>
+        <NavLink to='/' style={getNavlinkStyle}>
+          Home
+        </NavLink>
+
+        <NavLink to='/articles' style={getNavlinkStyle}>
+          Articles
+        </NavLink>
+      </nav>
+    </header>
+  );
 }
--- src/screens/Articles.js
+++ src/screens/Articles.js
@@ -1,3 +1,52 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 export default function Articles() {
-  return <>Artricles Page</>;
+  const [articleList, setArticleList] = useState([]);
+  const [loadingError, setLoadingError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setLoadingError('');
+    const controller = new AbortController();
+
+    console.log('montage du composant');
+
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts', {
+        signal: controller.signal,
+      })
+      .then((res) => {
+        setArticleList(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+        if (err.message !== 'canceled')
+          setLoadingError(
+            `Impossible de charger la liste d'articles depuis l'API`
+          );
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+
+    return () => {
+      console.log('demontage du composant Articles, annulation de la requête');
+      controller.abort();
+    };
+  }, []);
+
+  return (
+    <>
+      <h1>Les articles</h1>
+      {isLoading && <p>Chargment en cours...</p>}
+      <p style={{ color: 'red' }}>{loadingError}</p>
+      <ul>
+        {articleList.map((article) => (
+          <li key={article.id}>{article.title}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
